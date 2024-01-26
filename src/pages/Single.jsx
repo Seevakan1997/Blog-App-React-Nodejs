cd18648558e0e32add2cc4dcd05117c97e56ebbf
Seevakan1997 <seevakan1@gmail.com>
2024-01-26 23:06:58 +0530

    single post page updated

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,38 +1,67 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Edit from '../img/edit.png'
 import Delete from '../img/delete.png'
-import { Link } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Menu from '../components/Menu'
+import axios from 'axios'
+import moment from 'moment'
+import { AuthContext } from '../context/authContext';
 
 function Single() {
+  const [post,setPost] = useState({});
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const postId = location.pathname.split("/")[2];
+
+  const {currentUser}  = useContext(AuthContext);
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      try{
+        const res = await axios.get(`/posts/${postId}`)
+        setPost(res.data);
+      }catch(err){
+        console.log(err);
+      }
+    };
+    fetchData();
+  },[postId]);
+
+  const handleDelete = async ()=>{
+    try{
+      await axios.delete(`/posts/${postId}`)
+      navigate("/")
+    }catch(err){
+      console.log(err);
+    }
+  }
+
   return (
     <div className='single'>
       <div className='content'>
-        <img src='https://extension.umn.edu/sites/extension.umn.edu/files/Two%20apples%20close-up_screen.jpg'/>
+        <img src={post?.img}/>
         <div className='user'>
-          <img src='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80' alt=''/>
-        
+          {post.userImg &&<img src={post.userImg} alt=''/>
+        }
         <div className='info'>
-          <span>Hrithik</span>
-          <p>Posted 2 days ago</p>
+          <span>{post.username}</span>
+          <p>Posted {moment(post.date).fromNow()}</p>
         </div>
 
-        <div className='edit'>
+        {currentUser.username === post.username &&<div className='edit'>
           <Link to={`/write?edit=2`}>
             <img src={Edit} alt=''/>
           </Link>
-          <img src={Delete} alt=''/>
+          <img onClick={handleDelete} src={Delete} alt=''/>
+        </div>}
         </div>
-        </div>
-        <h1>Heloo dfveh sdhbh sdbhe dhsdhge effd</h1>
-        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-        <br/>
-        Why do we use it?
-        <br/>
-        It is  established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        <h1>{post.title}</h1>
+        <p>
+          {post.desc}
         </p>
       </div>
-      <Menu/>
+      <Menu cat={post.cat}/>
     </div>
   )
 }
